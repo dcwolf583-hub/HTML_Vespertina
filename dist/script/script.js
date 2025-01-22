da255a108448c735e9d221d52aa232c663a7fffb
DanielCampos <danycampos1100@gmail.com>
2025-01-22 16:08:13 -0600

    Ejercicio de login y registro para mostrar el nombre, fecha y hora.

---
+++ dist/script/script.js
@@ -0,0 +1,38 @@
+$("#btnEnviar").click(function() {
+    var nonbre = $("#txtNombre").val();
+    var email = $("#txtEmail").val();
+});
+
+/*Login */
+$("#btnLogin").click(function() {
+    var email = $("#txtEmail").val();
+    var password = $("#txtPassword").val();
+    var recordar = $("#chkRecordar").is(":checked");
+    
+    if(email !="Daniel" || password != "AdminAHM123"){
+        Swal.fire({
+            icon: "error",
+            title: "Error al ingresar",
+            text: "Usuario o contraseña incorrecta",
+          });
+    }else{
+        window.location.href = "ejercicio3.html";
+    }
+});
+
+/*Mostrar nombre, correo y fecha */
+$("#btnEnviar").click(function() {
+    var pNombre = $("#txtPNombre").val();
+    var sNombre = $("#txtSNombre").val();
+    var pApellido = $("#txtPApellido").val();
+    var sApellido = $("#txtSApellido").val();
+    var nombreCompleto = pNombre + " " + sNombre + " " + pApellido + " " + sApellido;
+    var email = $("#txtCorreo").val();
+    var fecha = $("#inpFechaYHora").val();
+
+    Swal.fire({
+        icon: "success",
+        title: "Datos del formulario",
+        html: "Bienvenido (a): " + nombreCompleto + "<br>Correo: " + email + "<br>Fecha de registro: " + fecha,
+      });
+});
